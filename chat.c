46472f87816d783bc5981a4f06d08feb4ad5819e
laur <duicul@yahoo.com>
2018-02-28 00:46:19 +0200

    3

--- chat.c
+++ chat.c
@@ -12,47 +12,44 @@
 #include <netdb.h>
 #include <errno.h>
 #define BUFF_SIZE 200
+
 int setaddr(struct sockaddr_in *addr,char inaddr[],u_int32_t a,short sinport) {
 struct hostent *h;
 memset((void*)addr,0,sizeof(addr));
 if(inaddr!=NULL)
 {h = gethostbyname(inaddr);
 if(h!=NULL)
-addr->sin_addr.s_addr=*(u_int32_t*)h->h_addr_list[0];
-}
+addr->sin_addr.s_addr=*(u_int32_t*)h->h_addr_list[0];}
 else
 addr->sin_addr.s_addr=a;
 addr->sin_family = AF_INET ;
 addr->sin_port = htons(sinport);
-return 0 ;
-}
+return 0 ;}
 
 void read_data(int sock,char buf[])
 {char c;
 int i=0,n;
 read(sock,&n,sizeof(int));
-while(read(sock,&c,1)>0&&i<BUFF_SIZE&&i<n)
-{printf("%c",c);
-buf[i++]=c;}
-buf[i]='\0';}
+printf("n=%d\n",n);
+read(sock,buf,n);
+buf[n]='\0';
+printf("%s\n",buf);}
 
-void send_data(int sock,char buf[],int len)
+void send_data(int sock,char buf[])
 {char c;
-int i=0,n=len;
+int i=0,n=strlen(buf);
 write(sock,&n,sizeof(int));
-while(write(sock,&buf[i++],1)>0&&i<BUFF_SIZE&&i<n);
-buf[i]='\0';}
-
+write(sock,buf,n);}
 
 int main(int argv,char *args[])
 {char name[30],line[200];
 int j;
 printf("%s\n",args[1]);
-//fgets(name,30,stdin);
 struct sockaddr_in loc,rem;
 socklen_t rlen=sizeof(rem);
 setaddr(&loc,NULL,INADDR_ANY,atoi(args[1]));
 int sockfd, newsockfd;
+//creare server
 printf("Messsages:\n");
 if ((sockfd=socket(AF_INET,SOCK_STREAM,0)) < 0)
    {printf ("error1 ...\n"); /*exit(1);*/}
@@ -63,18 +60,20 @@ printf("2%s\n",strerror(errno));
 if (listen(sockfd,10) < 0)
    {printf ("error3 ...\n"); /*exit(1);*/}
 printf("3%s\n",strerror(errno));
+//server primire cereri
 for (;;)
 {newsockfd=accept(sockfd,(struct sockaddr *)&rem,&rlen);/* blocare pentru asteptare cereri*/
  printf("4%s\n",strerror(errno));
-if (newsockfd < 0)
+  if (newsockfd < 0)
    printf ("error4 ...\n");
   else
-  {read_data(newsockfd,line);
-  if(fork()==0) /*deservire cerere*/
-  { printf("line: %s",line);
-  close (newsockfd);
-  exit(0);}
-if(strcmp(line,"~exit\n")==0)
- {printf("Finish\n");break;}
+  {if(fork()==0) /*deservire cerere*/
+    while(3){
+   read_data(newsockfd,line);
+   send_data(newsockfd,line);
+  printf("line: %s",line);
+  if(strcmp(line,"~exit\n")==0)
+  {close(newsockfd);exit(0);}}
 }}
+
 return 0;}
--- client.c
+++ client.c
@@ -19,17 +19,17 @@ void read_data(int sock,char buf[])
 {char c;
 int i=0,n;
 read(sock,&n,sizeof(int));
-while(read(sock,&c,1)>0&&i<BUFF_SIZE&&i<n)
-{printf("%c",c);
-buf[i++]=c;}
-buf[i]='\0';}
+printf("n=%d\n",n);
+read(sock,buf,n);
+buf[n]='\0';
+printf("%s\n",buf);}
 
-void send_data(int sock,char buf[],int len)
+void send_data(int sock,char buf[])
 {char c;
-int i=0,n=len;
+int i=0,n=strlen(buf);
+printf("%s %d\n",buf,n);
 write(sock,&n,sizeof(int));
-while(write(sock,&buf[i++],1)>0&&i<BUFF_SIZE&&i<n);
-buf[i]='\0';}
+write(sock,buf,n);}
 
 int setaddr(struct sockaddr_in *addr,char inaddr[],u_int32_t a,short sinport) {
 struct hostent *h;
@@ -48,50 +48,53 @@ addr->sin_port = htons(sinport);
 return 0 ;
 }
 
-void end_proc()
-{char aux[]="/exit";
-write(sockfd,aux,strlen(aux));
-exit(0);
-}
-
-void start_chat(char aux[],char exec[])
-{char val[100],pip[10];
-strcpy(val,"");
-strcat(val,exec);
-strcat(val,"\ ");
-strcat(val,aux);
-printf("%s",val);
-execlp("gnome-terminal","gnome-terminal","-e",val,NULL);}
-
 int main(int argv,char *args[])
 {char text[100];
-int j,pid,i;
+int j,val,pid,i,init=1;
+char user[100],pass[100];
 struct sockaddr_in loc,rem;
-if(argv>3&&(pid=fork())==0)
-{printf("args2 %s\n",args[2]);
-start_chat(args[2],args[3]);
-exit(1);
-}
-int val;
-do{fflush(stdin);
-fgets(text,100,stdin);
-for(i=0;i<20;i++)
-{setaddr(&loc,NULL,INADDR_ANY,0);
-if ((sockfd=socket(PF_INET,SOCK_STREAM,0)) < 0)
+char mess[100];
+do{
+if(init==1)
+{//conectare socket
+for(i=0;i<10;i++)
+{if ((sockfd=socket(PF_INET,SOCK_STREAM,0)) < 0)
    {printf ("error1 ...\n"); continue;}
 printf("1%s\n",strerror(errno));
-if ((bind(sockfd,(struct sockaddr *)&loc,sizeof(loc)))<0)
-   {printf ("error2 ...\n"); continue;}
 setaddr(&rem,args[1],0,atoi(args[2]));
 printf("con %d\n",val=connect(sockfd,(struct sockaddr *)&rem,sizeof(rem)));
 printf("2%s\n",strerror(errno));
 if(val<0)
 continue;
-send_data(sockfd,text,strlen(text));
+else {//creare proces copil=> primire mesaje
+    if(fork()==0)
+  while(3)
+  {read_data(sockfd,mess);
+   printf("Message: %s",mess);
+   if(strcmp(mess,"~exit\n")==0)
+   {close(sockfd);exit(0);}}}
+   init=0;break;}}
+
+//logare
+/*
+printf("User: ");
+scanf("%s",user);
+printf("Pass: ");
+scanf("%s",pass);
+send_data(sockfd,user);
+send_data(sockfd,pass);
+read_data(sockfd,mess);
+printf("Mess %s|\n",mess);
+init=strcmp(mess,"okay")==0 ? 0 : 1;*/
+   
+//trimiteremesaj
+fflush(stdin);
+fgets(text,100,stdin);
+send_data(sockfd,text);
 printf("sock %d %s",sockfd,text);
-//text[strlen(text-1)]='\0';
-close(sockfd);
-break;
+//iesire client
+if(strcmp(text,"~exit\n")==0)
+{close(sockfd);init=1;}
 }
-}while(strcmp(text,"~exit\n")!=0);
+while(3||strcmp(text,"~exit\n")!=0);
 return 0;}
